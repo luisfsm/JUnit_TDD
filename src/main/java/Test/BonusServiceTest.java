d98b072004fddba9ab119f60173c8e23c7af57e3
luis felipe <luis.s.mendes20@gmail.com>
2021-12-12 16:34:33 -0300

    Implementando primeiros teste com regra de negocios

---
+++ src/main/java/Test/BonusServiceTest.java
@@ -0,0 +1,42 @@
+package Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.BonusService;
+
+class BonusServiceTest {
+
+	@Test
+	@DisplayName("Verifica se salario ultrapassa o bonus de 1000 e o deixa 0")
+	void calcularBonusFuncionarioSalarioAlto() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Luis Felipe",LocalDate.now(), new BigDecimal(25000)));
+		assertEquals(new BigDecimal("0.00"), bonus);
+	}
+	
+	@Test 
+	void BonusDeveSerDeDezPorcentos() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Luis Felipe",LocalDate.now(), new BigDecimal(2500)));
+		
+		assertTrue(bonus.equals(new BigDecimal("250.00")));
+		
+	}
+	
+	@Test
+	void bonusDeveriaSerDezPorCentoDe10000() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Luis Felipe",LocalDate.now(), new BigDecimal(10000)));
+		assertEquals(new BigDecimal("1000.00"), bonus);
+		
+	}
+
+}
