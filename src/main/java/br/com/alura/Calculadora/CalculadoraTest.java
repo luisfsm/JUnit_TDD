70b5926c190ef2d44aeb257822b081a8911d415f
luis felipe <luis.s.mendes20@gmail.com>
2021-12-12 15:41:59 -0300

    primeiro teste com JUnit

---
+++ src/main/java/br/com/alura/Calculadora/CalculadoraTest.java
@@ -0,0 +1,18 @@
+package br.com.alura.Calculadora;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+public class CalculadoraTest {
+
+	@Test
+    public void deveriaSomarDoisNumerosInteiros(){
+        Calculadora calc =  new Calculadora();
+        int soma =  calc.somar(7, 3);
+        
+        //Assert Class Junit pra validar dados, JUnit.assertEquals verifica se os dados são iguais 
+        Assert.assertEquals(10, soma);
+    }
+    
+}
