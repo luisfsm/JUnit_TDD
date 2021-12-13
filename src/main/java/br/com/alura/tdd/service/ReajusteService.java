3fc8a50aa3adb2d54504d9ef13cbb96da90c6995
luis felipe <luis.s.mendes20@gmail.com>
2021-12-13 11:16:58 -0300

    Refactory nas classes de desempenho

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,30 +8,9 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
 	public void ConcederReajuste(Funcionario func, Desempenho desempenho) {
-		
-		switch(desempenho) {
-			case A_DESEJAR :
-				
-				BigDecimal reajuste = func.getSalario().multiply(new BigDecimal("0.03"));
-				func.reajustarSalario(reajuste);
-				
-				break;
-				
-			case BOM :
-				
-				BigDecimal reajuste1 = func.getSalario().multiply(new BigDecimal("0.15"));
-				func.reajustarSalario(reajuste1);
-				
-				break;
-				
-			case OTIMO :
-				
-				BigDecimal reajuste2 = func.getSalario().multiply(new BigDecimal("0.20"));
-				func.reajustarSalario(reajuste2);
-				
-				break;
-		}
-				
+				BigDecimal percentual = desempenho.percentualReajuste();
+				BigDecimal reajuste = func.getSalario().multiply(percentual);
+				func.reajustarSalario(reajuste);		
 	}
 
 }
